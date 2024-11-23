f98ee394f89e65aad0586503c3f4aec3bc874df7
keinyer <keinyeryguaran@gmail.com>
2024-11-23 12:05:44 -0400

    imprimiendo libros

--- libreria.js
+++ libreria.js
@@ -1,5 +1,21 @@
-let librosColeccion=[{
-
-
-    
+let librosColeccion=[
+{
+"nombre":" una divina comedia",
+"cantidadDePaginas":113,
+"auntor":"Dante Alighieri",
+"genero":["posia"],
+"disponible":true
+},
+{
+"nombre":"el caballero de armadura oxidada",
+"cantidadDePaginas":108,
+"autor":" Robert Fisher",
+"genero":"ficcion",
+"disponible":true
 }]
+function informacionLibro (libro){
+console.log("el libro"+  libro.nombre + " tiene " + libro.cantidadDePaginas+ " paginas, fue escrito por" + librosColeccion.auntor )
+
+}
+
+informacionLibro(librosColeccion[0])
