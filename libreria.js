1616776bec28c514fad4cfc41a27add41595c72a
pixelatrox <keinyeryguaran@gmail.com>
2024-12-01 14:18:47 -0400

    libros disponibles

--- libreria.js
+++ libreria.js
@@ -2,20 +2,26 @@ let librosColeccion=[
 {
 "nombre":" una divina comedia",
 "cantidadDePaginas":113,
-"auntor":"Dante Alighieri",
-"genero":["posia"],
-"disponible":true
+"autor":"Dante Alighieri",
+"genero":["poesia"],
+"disponible":false
 },
 {
 "nombre":"el caballero de armadura oxidada",
 "cantidadDePaginas":108,
 "autor":" Robert Fisher",
 "genero":"ficcion",
-"disponible":true
+"disponible":false
 }]
 function informacionLibro (libro){
-console.log("el libro"+  libro.nombre + " tiene " + libro.cantidadDePaginas+ " paginas, fue escrito por" + librosColeccion.auntor )
+    console.log("el libro"+  libro.nombre + " tiene " + libro.cantidadDePaginas+ " paginas , fue escrito por " + libro.autor + " y es del genero " + libro.genero  )
 
 }
 
-informacionLibro(librosColeccion[0])
+ 
+for (let i = 0; i < librosColeccion.length; i++) {
+    informacionLibro(librosColeccion[i]) }
+
+
+
+
