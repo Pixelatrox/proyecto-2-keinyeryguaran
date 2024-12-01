2f802ea54df79cfd89651cf9b0814e17f1d92800
pixelatrox <keinyeryguaran@gmail.com>
2024-12-01 16:05:46 -0400

    agregar libros y completar el disponoble

--- libreria.js
+++ libreria.js
@@ -7,21 +7,44 @@ let librosColeccion=[
 "disponible":false
 },
 {
-"nombre":"el caballero de armadura oxidada",
+"nombre":" el caballero de armadura oxidada",
 "cantidadDePaginas":108,
 "autor":" Robert Fisher",
 "genero":"ficcion",
-"disponible":false
+"disponible":true
 }]
 function informacionLibro (libro){
-    console.log("el libro"+  libro.nombre + " tiene " + libro.cantidadDePaginas+ " paginas , fue escrito por " + libro.autor + " y es del genero " + libro.genero  )
+    let mensaje ="el libro"+  libro.nombre + " tiene " + libro.cantidadDePaginas+ " paginas , fue escrito por " + libro.autor + " y es del genero " + libro.genero ;
+    if (libro.disponible === true){ mensaje += " y esta disonible"}
+    else{mensaje += " y no esta disonible"}    
+            console.log(mensaje)
 
+    
+        
 }
+function imprimirLibros(libros) {
+for (let i = 0; i < libros.length; i++) {
+    informacionLibro(libros[i]) 
+    
+}
+}
+let nuevoLibro = {
+    nombre: " Cien años de soledad",
+    cantidadDePaginas: 417,
+    autor: "Gabriel García Márquez",
+    genero: ["ficcion", "realismo mágico"],
+    disponible: true
+};
 
- 
-for (let i = 0; i < librosColeccion.length; i++) {
-    informacionLibro(librosColeccion[i]) }
+informacionLibro(nuevoLibro);
+
+function agregarLibro(libros, nuevoLibro) {
+    libros.push(nuevoLibro);
+}
+    agregarLibro(librosColeccion, nuevoLibro);
+imprimirLibros(librosColeccion);
 
 
+      
 
 
